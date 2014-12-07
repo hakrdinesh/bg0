5f3a58460406c2cdd99cf36a5f57555a5b62edbd
Sai Sivam <dinesh.thirumurthy@gmail.com>
2014-12-07 13:37:08 +0530

    20141207 class

--- slokas.py
+++ slokas.py
@@ -13,22 +13,35 @@ for line in file:
 	if line.startswith("#"):
 		print(line)
 		continue
-	if "." not in line:
-		print(line)
-		continue
+
 	if line.startswith("CC"):
 		ccm = True
 		sloka_string = line[3:]
 		sloka_id = sloka_string.strip()
 		if debug:
 			print("SLOKA string", sloka_id)
+	elif line.startswith("ISO"):
+		ccm = False
+		iso = True
+		sloka_string = line[4:]
+		sloka_id = sloka_string.strip()
+		if debug:
+			print("SLOKA string", sloka_id)
 	else:
 		ccm = False
+		iso = False
 		sloka_id = line
 	
 	components = sloka_id.split(".")
+	if debug:
+		print(components)
 	ndots = len(components)-1
-	if (ndots == 1):
+	if (ndots == 0):
+		if iso:
+			sastra = "iso"
+		else:
+			sastra = ""
+	elif (ndots == 1):
 		if ccm:
 			if line.startswith("CCM"):
 				sastra = "cc/madhya"
@@ -42,9 +55,13 @@ for line in file:
 		sastra = "sb"
 	else:
 		sastra = "something"
-	
-	suffix = "/".join(components)
-	nline = line + "\t" + "http://vedabase.com/en/" + sastra + "/" + suffix
+
+	if sastra:
+		suffix = "/".join(components)
+		nline = line + "\t" + "http://vedabase.com/en/" + sastra + "/" + suffix
+	else:
+		suffix = ""
+		nline = ""
 	if nline.endswith("P"):
 		nline = nline[:-1]
 		nline = nline + " " + "read purport"
